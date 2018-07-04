c70e915fecbd8b3640aab364540ad4d0854b60e8
Juliardi <juliardi.hidayatullah@tokopedia.com>
2018-07-04 16:37:05 +0700

    Add vendor and modified config.yml

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	b := add(10, 20)
-	fmt.Println(b)
+	_, err := sqlx.Connect("postgres", "user=postgres dbname=postgres password=root sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 // simple addition math func
